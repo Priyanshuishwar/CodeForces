30960f9aa8aab1160d2b764460d08b6fd1eaeb96
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-08-24 08:10:23 +0530

    Add Arpa’s hard exam and Mehrdad’s naive cheat [A] from Codeforces

---
+++ 742/A - Arpa’s hard exam and Mehrdad’s naive cheat/solution.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Main{	public static void main(String[] args) {		Scanner sc = new Scanner(System.in);		int n = sc.nextInt();		int cycle[] = {8,4,2,6};				if(n == 0){		    System.out.println("1");		}		else{		    int ans = cycle[(int)(n - 1) % 4];		    System.out.print(ans);		}	}}
