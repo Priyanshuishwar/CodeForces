1e1453173b2333469eecdfe600fb90c124ec7863
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-08-29 08:21:38 +0530

    Add Ilya and Bank Account [A] from Codeforces

---
+++ 313/A - Ilya and Bank Account/solution.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Main{	public static void main(String[] args) {		Scanner sc = new Scanner(System.in);		int n = sc.nextInt();		if(n >= 0){		    System.out.print(n);		}		else{		    int last = n / 10;		    int rem = n % 10;		    int sec = (n / 100 ) * 10 + rem;		    System.out.print(Math.max(last,sec));		}	}}
