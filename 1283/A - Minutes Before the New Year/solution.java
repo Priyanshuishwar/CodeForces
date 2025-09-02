71807a24c655064d3eb81973d30f73fcda123335
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2025-09-02 07:50:33 +0530

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Main{	public static void main(String[] args) {		Scanner sc = new Scanner(System.in);		int t = sc.nextInt();		while(t -- > 0){		    int hr = sc.nextInt();		    int min = sc.nextInt();		    int orig = 1440;		    		    int cal = hr * 60 + min;		    int ans = orig - cal;		    System.out.println(ans);		}	}}
