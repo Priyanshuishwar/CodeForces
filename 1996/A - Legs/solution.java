1a7f76b2d4ce5e5c370d3a69197a9c0d27449f0c
Priyanshu Kumar <140679451+Priyanshuishwar@users.noreply.github.com>
2026-05-19 20:27:41 -0700

    Add Legs [A] from Codeforces

---
+++ 1996/A - Legs/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int animals = 0;
+		    while(n > 0){
+		        animals += n / 4;
+		        n = n % 4;
+		        
+		        animals += n / 2;
+		        n = n % 2;
+		    }
+		    System.out.println(animals);
+		}
+		
+	}
+}
